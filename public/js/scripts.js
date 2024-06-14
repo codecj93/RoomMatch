658048a3b1910ed8d755862da15c0ff2fd64bcf0
Avatar <codecj93@gmail.com>
2024-06-13 21:18:37 -0400

    code added to script.js to POST register informetion to db

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,3 +1,49 @@
+const registerForm = document.querySelector("#register");
+
+registerForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    const firstname = document.querySelector("#firstname").value;
+    const lastname = document.querySelector("#lasname").value;
+    const password = document.querySelector("#password").value;
+    const email = document.querySelector("#password").value;
+    const city = document.querySelector("#city").value;
+    const state = document.querySelector("#password").value;
+    const country = document.querySelector("#country").value;
+   
+
+
+    fetch("/api/users/", {
+        method: "POST",
+        body: JSON.stringify({
+            firstname,
+            lastname,
+            email,
+            password,
+            city,
+            state,
+            country
+        }),
+        headers: {
+            'Content-Type': "application/json"
+        }
+    })
+    .then((res) => {
+        if(res.status == 200) {
+            console.log("User signup successful!")
+            window.location.href = "/dashboard"
+        } else if(res.status == 400) {
+            console.log('Something went wrong!')
+        }
+    })
+})
+
+
+
+
+
+
+
 document.addEventListener("DOMContentLoaded", function() {
     const profileForm = document.getElementById("profileForm");
     if (profileForm) {
